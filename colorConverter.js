1e73d6480f4d4bcbcd6dcd496009be854d15cc12
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-11-17 23:33:21 -0400

    Update colorConverter.js

--- colorConverter.js
+++ colorConverter.js
@@ -14,17 +14,15 @@ function hsv2rgb(h,s,v){
   const x = c * (1 - Math.abs(hprime % 2 - 1));
   const m = v - c;
   let r, g, b;
-  if (!hprime) {r = 0; g = 0; b = 0; }
-  if (hprime >= 0 && hprime < 1) { r = c; g = x; b = 0}
-  if (hprime >= 1 && hprime < 2) { r = x; g = c; b = 0}
-  if (hprime >= 2 && hprime < 3) { r = 0; g = c; b = x}
-  if (hprime >= 3 && hprime < 4) { r = 0; g = x; b = c}
-  if (hprime >= 4 && hprime < 5) { r = x; g = 0; b = c}
-  if (hprime >= 5 && hprime < 6) { r = c; g = 0; b = x}
+  if (isNaN(hprime)) {r = 0; g = 0; b = 0; }
+  switch (Math.floor(hprime)) {
+    case 0: { r = c; g = x; b = 0; break}
+    case 1: { r = x; g = c; b = 0; break}
+    case 2: { r = 0; g = c; b = x; break}
+    case 3: { r = 0; g = x; b = c; break}
+    case 4: { r = x; g = 0; b = c; break}
+    case 5: { r = c; g = 0; b = x; break}
+  }
 
-  r = Math.round( (r + m)* 255);
-  g = Math.round( (g + m)* 255);
-  b = Math.round( (b + m)* 255);
-
-  return [r, g, b]
+  return [r, g, b].map(x => Math.round( (x + m)* 0xff))
 }
